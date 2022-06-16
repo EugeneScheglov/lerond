888e90e90ec8989b20fbda3204df9cbde612e9dc
Евгений <83172101+EugeneScheglov@users.noreply.github.com>
2022-06-16 18:28:53 +0300

    slider

--- utils/script.js
+++ utils/script.js
@@ -1,7 +1,7 @@
-document.querySelector('.header__menu-button').addEventListener('click', () => {
+const PopupOpen = document.querySelector('.header__menu-button').addEventListener('click', () => {
     document.querySelector('.popup').classList.toggle('popup_opened');
 });
 
-document.querySelector('.popup__close').addEventListener('click', () => {
+const PopupClose = document.querySelector('.popup__close').addEventListener('click', () => {
     document.querySelector('.popup').classList.remove('popup_opened');
 });
